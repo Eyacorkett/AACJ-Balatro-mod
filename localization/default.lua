53854d2e37fda84216bfe92225fc141df5d8acfa
Eyacorkett <121724364+Eyacorkett@users.noreply.github.com>
2025-05-26 02:41:01 +0800

    Add files via upload

--- localization/default.lua
+++ localization/default.lua
@@ -2,60 +2,604 @@ return {
     descriptions = {
         alt_texture = {
 			alt_tex_AACJ_aacj = {
-				j_raised_fist = {
+			j_joker = {
+					name = "Jimbo"
+				},
+				
+			j_greedy_joker = {
+					name = "Thalassa Gramarye"
+				},
+				
+			j_lusty_joker = {
+					name = "Valant Gramarye"
+				},
+				
+			j_wrathful_joker = {
+					name = "Magnifi Gramarye"
+				},
+				
+			j_gluttenous_joker = {
+					name = "Zak Gramarye"
+				},
+				
+			j_jolly = {
+					name = "Rodrigo Starwalker"
+				},
+				
+			j_zany = {
+					name = "Marie"
+				},
+				
+			j_mad = {
+					name = "Saber"
+				},
+				
+			j_crazy = {
+					name = "Herlock Sholmes"
+				},
+				
+			j_droll = {
+					name = "Roger Retinz"
+				},
+				
+			j_sly = {
+					name = "Freaky Vex Vulper"
+				},
+				
+			j_wily = {
+					name = "Dog Race"
+				},
+				
+			j_clever = {
+					name = "Ladder"
+				},
+				
+			j_devious = {
+					name = "Horse"
+				},
+				
+			j_crafty = {
+					name = "Minecraft Steve"
+				},
+				
+			j_half = {
+					name = "Erasing Evidence"
+				},
+				
+			j_stencil = {
+					name = "House, M.D."
+				},
+				
+			j_four_fingers = {
+					name = "Loss"
+				},
+				
+			j_mime = {
+					name = "Agatha Christie"
+				},
+				
+			j_credit_card = {
+					name = "Republic of Borginia"
+				},
+				
+			j_ceremonial = {
+					name = "Hunger Games"
+				},
+				
+			j_banner = {
+					name = "Celeste McCoy Flag"
+				},
+				
+			j_mystic_summit = {
+					name = "Peter Griffin"
+				},
+				
+			j_marble = {
+					name = "Whax Apper"
+				},
+				
+			j_loyalty_card = {
+					name = "Danganronpa: Trigger Happy Havoc"
+				},
+				
+			j_8_ball = {
+					name = "Mudae"
+				},
+				
+			j_misprint = {
+					name = "Unfortunate Typo"
+				},
+				
+			j_dusk = {
+					name = "Sebastian Debeste"
+				},
+				
+			j_raised_fist = {
 					name = "Objection!"
 				},
 				
-				j_chaos = {
+			j_chaos = {
 					name = "Mencken"
 				},
 				
-				j_scholar = {
+			j_fibonacci = {
+					name = "Jakkid166"
+				},
+				
+			j_steel_joker = {
+					name = "Tony Nubby"
+				},
+				
+			j_scary_face = {
+					name = "Mario"
+				},
+				
+			j_abstract = {
+					name = "Saul Goodman"
+				},
+				
+			j_delayed_grat = {
+					name = "Alex Eggleston"
+				},
+				
+			j_hack = {
+					name = "You're a Goner!"
+				},
+				
+			j_pareidolia = {
+					name = "Larry Butz"
+				},
+				
+			j_gros_michel = {
+					name = "Collin Aries"
+				},
+				
+			j_even_steven = {
+					name = "Metis Cookie"
+				},
+				
+			j_odd_todd = {
+					name = "Aura Cookie"
+				},
+				
+			j_scholar = {
 					name = "Wesley Stickler"
 				},
 				
-				j_faceless = {
-					name = "Inga Karkhuul Haw'kohd Dis'nahm Bi'ahni Lawga Ormo Pohmpus Da'nit Ar'edi Iz Khura'in III"
+			j_business = {
+					name = "Maya's Card"
 				},
 				
-				j_green_joker = {
+			j_supernova = {
+					name = "Ryunosuke's Death"
+				},
+				
+			j_ride_the_bus = {
+					name = "Mitchell Shepherd"
+				},
+				
+			j_space = {
+					name = "It's Fantastic"
+				},
+				
+			j_egg = {
+					name = "Garfield Kart"
+				},
+				
+			j_burglar = {
+					name = "Maya Plush"
+				},
+				
+			j_blackboard = {
+					name = "Quiplash"
+				},
+				
+			j_runner = {
+					name = "Cove Holden"
+				},
+				
+			j_ice_cream = {
+					name = "AotA"
+				},
+				
+			j_dna = {
+					name = "Yggdrasil"
+				},
+				
+			j_splash = {
+					name = "@frorf ping role III"
+				},
+				
+			j_blue_joker = {
+					name = "Goodra Plush"
+				},
+				
+			j_sixth_sense = {
+					name = "Orga Orly"
+				},
+				
+			j_constellation = {
+					name = "Metis Cykes"
+				},
+				
+			j_hiker = {
+					name = "Crazy Cattle 3D"
+				},
+				
+			j_faceless = {
+					name = "Inga Karkhuul Khura'in"
+				},
+				
+			j_green_joker = {
 					name = "Green Mode"
 				},
 				
-				j_photograph = {
-					name = "Seal"
+			j_superposition = {
+					name = "Shu Takumi"
 				},
 				
-				j_turtle_bean = {
+			j_todo_list = {
+					name = "Molars"
+				},
+				
+			j_cavendish = {
+					name = "Collin Aries Ver. 2.0"
+				},
+				
+			j_card_sharp = {
+					name = "Ema Skye"
+				},
+				
+			j_red_card = {
+					name = "Red Card"
+				},
+				
+			j_madness = {
+					name = "Deid Mann"
+				},
+				
+			j_square = {
+					name = "Purple Guy"
+				},
+				
+			j_seance = {
+					name = "Divination Séance"
+				},
+				
+			j_riff_raff = {
+					name = "Aristotle Means"
+				},
+				
+			j_vampire = {
+					name = "Grick"
+				},
+				
+			j_shortcut = {
+					name = "Jove Justice"
+				},
+				
+			j_hologram = {
+					name = "Mood Matrix"
+				},
+				
+			j_vagabond = {
+					name = "Dick Gumshoe"
+				},
+				
+			j_baron = {
+					name = "Damon Gant"
+				},
+				
+			j_cloud_9 = {
+					name = "9 Hours, 9 Persons, 9 Doors"
+				},
+				
+			j_rocket = {
+					name = "Turnabout of the Snow"
+				},
+				
+			j_obelisk = {
+					name = "Bananafries"
+				},
+				
+			j_midas_mask = {
+					name = "Paul Atishon"
+				},
+				
+			j_luchador = {
+					name = "Hot Purple Pikmin"
+				},
+				
+			j_photograph = {
+					name = "Video Game About Depression"
+				},
+				
+			j_gift = {
+					name = "Keiji Kiyowai"
+				},
+				
+			j_turtle_bean = {
 					name = "Pea"
 				},
 				
-				j_bull = {
+			j_erosion = {
+					name = "Stretched Shuichi"
+				},
+				
+			j_reserved_parking = {
+					name = "Gartic Phone"
+				},
+				
+			j_mail = {
+					name = "Celeste McCoy"
+				},
+				
+			j_to_the_moon = {
+					name = "Bald Teto"
+				},
+				
+			j_hallucination = {
+					name = "Hallucination"
+				},
+				
+			j_fortune_teller = {
+					name = "Uendo Toneido"
+				},
+				
+			j_juggler = {
+					name = "Ragna the Bloodedge"
+				},
+				
+			j_drunkard = {
+					name = "The Devil"
+				},
+				
+			j_stone = {
+					name = "Hideo Kojima"
+				},
+				
+			j_golden = {
+					name = "Thanks for the Gold!"
+				},
+				
+			j_lucky_cat = {
+					name = "Neco-Arc"
+				},
+				
+			j_baseball = {
+					name = "Jacque Koff"
+				},
+				
+			j_bull = {
 					name = "Neem"
 				},
 				
-				j_trading = {
+			j_diet_cola = {
+					name = "John Marsh's Milk"
+				},
+				
+			j_trading = {
 					name = "Burnout 3"
 				},
 				
-				j_ramen = {
+			j_flash = {
+					name = "Shoe"
+				},
+				
+			j_popcorn = {
+					name = "MEE6"
+				},
+				
+			j_trousers = {
+					name = "Magolor"
+				},
+				
+			j_ancient = {
+					name = "Protagonist (AAI0)"
+				},
+				
+			j_ramen = {
 					name = "Burgers"
 				},
 				
-				j_walkie_talkie = {
+			j_walkie_talkie = {
 					name = "Shelly de Killer"
 				},
 				
-				j_campfire = {
+			j_selzer = {
+					name = "Kay Faraday"
+				},
+				
+			j_castle = {
+					name = "Roller Coaster"
+				},
+				
+			j_smiley = {
+					name = ":face_holding_back_tears:"
+				},
+				
+			j_campfire = {
 					name = "Meowl"
 				},
 				
-				j_hanging_chad = {
+			j_ticket = {
+					name = "Gyakuten Saiban"
+				},
+				
+			j_mr_bones = {
+					name = "Takeshi Yamazaki"
+				},
+				
+			j_acrobat = {
+					name = "Freaky Simon Keyes"
+				},
+				
+			j_sock_and_buskin = {
+					name = "Monokuma"
+				},
+				
+			j_swashbuckler = {
+					name = "Nekotina"
+				},
+				
+			j_troubadour = {
+					name = "Hervis Daubeny"
+				},
+				
+			j_certificate = {
+					name = "Seal"
+				},
+				
+			j_smeared = {
+					name = "The Great Mr. Reus"
+				},
+				
+			j_throwback = {
+					name = "Meowl's Dog"
+				},
+				
+			j_hanging_chad = {
 					name = "Calc"
 				},
 				
-				j_rough_gem = {
+			j_rough_gem = {
 					name = "Absolute Cutscene"
+				},
+				
+			j_bloodstone = {
+					name = "Grug"
+				},
+				
+			j_arrowhead = {
+					name = "Zoo-Wee Mama!"
+				},
+				
+			j_onyx_agate = {
+					name = "July"
+				},
+				
+			j_glass = {
+					name = "Tupperbot"
+				},
+				
+			j_ring_master = {
+					name = "Hifumi Yamada"
+				},
+				
+			j_flower_pot = {
+					name = "The Troupe's Legacy"
+				},
+				
+			j_blueprint = {
+					name = "Blueprint"
+				},
+				
+			j_wee = {
+					name = "Glue Joker"
+				},
+				
+			j_merry_andy = {
+					name = "Crythena"
+				},
+				
+			j_oops = {
+					name = "Fantastic Six"
+				},
+				
+			j_idol = {
+					name = "Tohru Adachi"
+				},
+				
+			j_seeing_double = {
+					name = "PyWright"
+				},
+				
+			j_matador = {
+					name = "Fiftyset"
+				},
+				
+			j_hit_the_road = {
+					name = "Jacking Off"
+				},
+				
+			j_duo = {
+					name = "Bonny & Betty"
+				},
+				
+			j_trio = {
+					name = "Turnabout Academy"
+				},
+				
+			j_family = {
+					name = "Colias Palaeno"
+				},
+				
+			j_order = {
+					name = "Nahyuta Sahdmadhi"
+				},
+				
+			j_tribe = {
+					name = "Baby Apollo"
+				},
+				
+			j_stuntman = {
+					name = "Kasane Pearto"
+				},
+				
+			j_invisible = {
+					name = "Meg Griffin"
+				},
+				
+			j_brainstorm = {
+					name = "Brainstorm"
+				},
+				
+			j_satellite = {
+					name = "Ramon"
+				},
+				
+			j_shoot_the_moon = {
+					name = "Emilia"
+				},
+				
+			j_drivers_license = {
+					name = "Apollo's Crotch"
+				},
+				
+			j_cartomancer = {
+					name = "David Tennant"
+				},
+				
+			j_astronomer = {
+					name = "Ga'ran Sigatar Khura'in"
+				},
+				
+			j_burnt = {
+					name = "Party in the C.I.A."
+				},
+				
+			j_bootstraps = {
+					name = "Prick Gumshoe"
+				},
+				
+			j_caino = {
+					name = "Duncan Seaman"
+				},
+				
+			j_triboulet = {
+					name = "Richard Wellington"
+				},
+				
+			j_yorick = {
+					name = "Ginger OC"
+				},
+				
+			j_chicot = {
+					name = "Vex Vulper"
+				},
+				
+			j_perkeo = {
+					name = "Quercus Alba"
 				},
 			},
 		},
